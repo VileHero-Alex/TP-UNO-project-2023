19106da85649957b28290fd55f9790be32e8785c
Николай Лисица <imsofoxyy@gmail.com>
2023-03-20 18:09:40 +0300

    Added 'uno' and 'draw' types to cards

--- card.py
+++ card.py
@@ -7,7 +7,7 @@ class Card:
 
     def cardIdentificator(self, id):
         color_pool = ["red", "yellow", "green", "blue", "black"]
-        type_pool = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "skip", "reverse", "+2", "choose", "+4"]
+        type_pool = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "skip", "reverse", "+2", "choose", "+4", "uno", "draw"]
         
         color = color_pool[id // 25]
         
@@ -17,7 +17,16 @@ class Card:
             type = "+4"
         elif 100 <= id <= 103:
             type = "choose"
+        elif id == 108:
+            type = "uno"
+        elif id == 109:
+            type = "draw"
         else:
             type = type_pool[(id % 25 + 1) // 2] 
 
         return (color, type)
+
+
+if __name__ == "__main__":
+    for i in range(110):
+        print(i, Card(i).type, Card(i).color)
