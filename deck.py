7e71709798e7ce16f60317db629814fe6254cb82
BeyondSingularity <imsofoxyy@gmail.com>
2023-03-21 00:25:08 +0300

    all functions that returned card_id now return Card

--- deck.py
+++ deck.py
@@ -10,14 +10,14 @@ class Deck:
     def __len__(self):
         return len(self.cards)
     
-    def pop_card(self, pop_card_id):
+    def pop_card(self, pop_card_id) -> Card:
         try:
             self.cards.remove(pop_card_id)
-            return pop_card_id
+            return Card(pop_card_id)
         except:
             raise PopCardError("illegal move: there is no card that you are looking for!")
 
-    def receive_card(self, receive_card_id):
+    def receive_card(self, receive_card_id) -> None:
         self.cards.append(receive_card_id)
 
 
@@ -26,14 +26,14 @@ class DrawDeck(Deck):
         self.cards = [i for i in range(108)]
         self.shuffle()
 
-    def shuffle(self):
+    def shuffle(self) -> None:
         random.shuffle(self.cards)
     
-    def pop_top(self):
+    def pop_top(self) -> Card:
         received_card_id = self.pop_card(self.cards[0])
-        return received_card_id
+        return Card(received_card_id)
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return len(self.cards) == 0
 
 class TableDeck(Deck):
@@ -41,19 +41,19 @@ class TableDeck(Deck):
         super().__init__()
         self.top_color = None
 
-    def show_last(self):
+    def show_last(self) -> Card:
         if self.is_empty():
             return None
-        return self.cards[-1]
+        return Card(self.cards[-1])
     
-    def clear(self):
+    def clear(self) -> None:
         last_card_id = self.cards[-1]
         self.cards.pop()
         removed_cards = self.cards.copy()
         self.cards = [last_card_id]
         return removed_cards
     
-    def receive_card(self, receive_card_id):
+    def receive_card(self, receive_card_id) -> None:
         self.top_color = Card(receive_card_id).color
         self.cards.append(receive_card_id)
 
@@ -63,10 +63,10 @@ class PlayerDeck(Deck):
         scr = Card.system_card_range()
         self.cards = [i for i in range(scr[0], scr[1])] # system cards
 
-    def sort(self):
+    def sort(self) -> None:
         self.cards.sort()
 
-    def throw_card(self, throw_card_id):
+    def throw_card(self, throw_card_id) -> None:
         try:
             return self.pop_card(throw_card_id)
         except PopCardError as err:
