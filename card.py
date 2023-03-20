ef3104745f2307f6fda50645ac8e2bdaa3bd5cf6
Николай Лисица <imsofoxyy@gmail.com>
2023-03-20 20:23:19 +0300

    I think info about system cards should be put into a separate function as well + snake_case

--- card.py
+++ card.py
@@ -1,11 +1,11 @@
 class Card:
     def __init__(self, id):
         self.id = id
-        color, type = self.cardIdentificator(id)
+        color, type = self.card_identificator(id)
         self.color = color
         self.type = type
 
-    def cardIdentificator(self, id):
+    def card_identificator(id):
         color_pool = ["red", "yellow", "green", "blue", "black"]
         type_pool = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "skip", "reverse", "+2"]
         type_pool_extra = ["uno", "draw", "red", "yellow", "green", "blue"]
@@ -24,6 +24,9 @@ class Card:
             type = type_pool[(id % 25 + 1) // 2] 
 
         return (color, type)
+
+    def system_cards_range():
+        return (108, 114)
 
 
 if __name__ == "__main__":
