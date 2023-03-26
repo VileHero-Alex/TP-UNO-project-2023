dd75b280bc7c594fe27e5ec44441379633c9e71d
Николай Лисица <imsofoxyy@gmail.com>
2023-03-26 14:55:36 +0300

    moved modules to src

--- constants.py
+++
@@ -1,5 +0,0 @@
-HEADER = 64
-PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "beep boop, disconnected"
-SERVER = "localhost"
--- src/client.py
+++ src/client.py
@@ -1,8 +1,16 @@
-import socket
 import threading
-from constants import *
 from collections import deque
-from player import Player
+import socket
+
+import os
+import configparser
+
+config_file_path = os.path.join(os.path.dirname(__file__), '..', 'config.ini')
+config = configparser.ConfigParser()
+config.read(config_file_path)
+HEADER = config.getint('SYSTEM CONFIG', 'HEADER')
+FORMAT = config.get('SYSTEM CONFIG', 'FORMAT')
+DISCONNECT_MESSAGE = config.get('SYSTEM CONFIG', 'DISCONNECT_MESSAGE')
 
 class Client:
     def __init__(self, deque_lock: threading.Lock, *, conn=None, server=None, port=None, name=''):
@@ -51,52 +59,15 @@ class Client:
                     connected = False
                 self.deque_append(msg)
         self.conn.close()
+        self.thread.join()
     
     def deque_append(self, msg):
         with self.deque_lock:
             self.deque.append(msg)
     
     def deque_popleft(self):
-        with self.deque_lock:
-            return self.deque.popleft()
-
-
-class Server():
-    def __init__(self, server, port, deque_lock=None):
-        self.server = server
-        self.port = port
-        if deque_lock:
-            self.deque_lock = deque_lock
-        else:
-            self.deque_lock = threading.Lock()
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        while True:
-            try:
-                self.socket.bind((self.server, self.port))
-                break
-            except OSError:
-                self.port += 1
-        self.clients = []
-        self.socket.listen(1)
-        self.thread = threading.Thread(target=self.wait_for_connections)
-        self.thread.start()
-        print("[SERVER STARTUP] server is running on {}:{}".format(self.server, self.port))
-        
-    def wait_for_connections(self):
-        while True:
-            conn, addr = self.socket.accept()
-            self.clients.append(Player(self.deque_lock, conn=conn))
-            print(f"[NEW CONNECTION] {addr} connected.")
-            # print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 2}")
-
-
-if __name__ == '__main__':
-    port = 5050
-    server = Server(SERVER, port)
-    port = server.port
-    
-    while True:
-        for client in server.clients:
-            while len(client.deque) > 0:
-                message = client.deque_popleft()
-                print(client.name, ": ", message, sep='')
+        try:
+            with self.deque_lock:
+                return self.deque.popleft()
+        except IndexError:
+            return None
